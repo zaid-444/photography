ab4b982621b591eab9b78555c89c7bc682426055
Zaid <zaid0999@gmail.com>
2026-08-16 17:07:44 +0530

    Add STATIC_ROOT setting in settings.py for production collectstatic

--- vikas_photography/vikas_photography/settings.py
+++ vikas_photography/vikas_photography/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[STATIC_DIR]
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
